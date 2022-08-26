709a56ae9278d856d578c068b22b1f639de05205
condor0010 <condor0010@laptop>
2022-08-26 17:06:43 -0400

    just relised he gave us these

--- receiver.py
+++ receiver.py
@@ -1,3 +1,9 @@
-#!/bin/python
-import scapy.all as s
+from scapy.all import *
 
+def pkt_callback(pkt):
+    if pkt[ICMP].type==8:
+        print("[+] Received Message: "+str(pkt[Raw].load))
+
+print("[+] Started Listener")
+
+sniff(iface="wlan0",prn=pkt_callback,filter='icmp',store=0)
--- sender.py
+++ sender.py
@@ -1,3 +1,9 @@
-#!/bin/python
-import scapy.all as s
+from scapy.all import *
 
+conf.iface="wlan0"
+
+dst = "192.168.1.100"
+msg = "adastra per explotium"
+
+pkt = IP(dst=dst)/ICMP()/Raw(load=msg)
+send(pkt)

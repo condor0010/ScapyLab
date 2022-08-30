7f59c6eab141375a2289ecd95aff55b1838bf1ef
condor0010 <condor0010@laptop>
2022-08-29 23:53:41 -0400

    will fix in class tomorow

--- sender.py
+++ sender.py
@@ -1,21 +1,19 @@
 #!/bin/python
 from scapy.all import *
+from operator import xor
+import os
 
-# set interface to wlp4s0
-conf.iface="wlp4s0"
+conf.iface="wlp4s0" # set interface to wlp4s0
+conf.verb = 0 # stop scapy from printing junk
 
-# stop scapy from printing junk
-conf.verb = 0
-
-# set source and destination adresses
-mac_src = "00:00:00:00:00:00"
-mac_dst = "ff:ff:ff:ff:ff:ff"
+mac_src = "00:00:00:00:00:00" # set source mac adress
+mac_dst = "ff:ff:ff:ff:ff:ff" # set destination mac adress
 
 # make ethernet headder
 ether = Ether(src=mac_src, dst=mac_dst)
 
 def mk_pkt(lttr):
-    ip_addr = "0.0.0."+str(ord(lttr))
+    ip_addr = "10.10.200."+str(ord(lttr))
     arp = ARP(pdst=ip_addr) # make arp request
     packet = ether/arp # make packet
     sendp(packet) # send packet
@@ -25,3 +23,10 @@ msg = "horney goat weed"
 
 for lttr in msg:
     mk_pkt(lttr)
+
+
+# TODO
+# xor thing falco did, or something more complex
+# replace msg varible with data stream from stdin Ex: cat boobs.txt | ./sender.py
+# do recv file, works in wireshark so it should be fine
+# pep 8 stuff i guess, mostly just want it to look nice and be easy to read
